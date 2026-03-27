7af29eca89eb2362d7eb061de9d15513a0a362f8
Yun Junseo <yjs2673@naver.com>
2026-03-27 17:00:25 +0900

    packet handler 정의

---
+++ PacketHandler.cs
@@ -0,0 +1,25 @@
+using System;
+using MyGameServer.Models;
+
+public class PacketHandler
+{
+    public static void C_MoveHandler(Session session, IPacket packet)
+    {
+        C_Move movePacket = packet as C_Move;
+        if (movePacket == null || session == null) return;
+
+        // S_Move 패킷 생성 (브로드캐스트용)
+        S_Move res = new S_Move
+        {
+            playerId = session.SessionId, 
+            posX = movePacket.posX,
+            posY = movePacket.posY,
+            posZ = movePacket.posZ,
+            rotY = movePacket.rotY
+        };
+
+        // GameRoom 혹은 ChatServer의 브로드캐스트 로직 호출
+        // session을 인자로 넘겨 '나'를 제외하고 보낼 수 있게
+        GameRoom.Instance.Broadcast(res.Write(), session);
+    }
+}
